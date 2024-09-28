974d6bbd38852fe2648e3df7b45b7943bf8fedde
0lbap <pablo.laviron@etu.umontpellier.fr>
2024-09-28 22:42:48 +0200

    feat: Colors in CLI and new call graph command dependencies

--- evo-cli/build.gradle.kts
+++ evo-cli/build.gradle.kts
@@ -21,6 +21,9 @@ dependencies {
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("guru.nidi:graphviz-java:0.18.1")
+    implementation("org.apache.logging.log4j:log4j-core:2.24.0")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
 }
 
 tasks.test {
--- evo-cli/src/main/java/fr/umontpellier/evo/EvoCommand.java
+++ evo-cli/src/main/java/fr/umontpellier/evo/EvoCommand.java
@@ -1,7 +1,15 @@
 package fr.umontpellier.evo;
 
+import fr.umontpellier.evo.utils.Colors;
 import fr.umontpellier.evo.visitor.FileTreeAgregationVisitor;
 import fr.umontpellier.evo.visitor.StatisticVisitor;
+import guru.nidi.graphviz.attribute.Color;
+import guru.nidi.graphviz.attribute.Font;
+import guru.nidi.graphviz.attribute.Rank;
+import guru.nidi.graphviz.attribute.Style;
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
+import guru.nidi.graphviz.model.Graph;
 import picocli.CommandLine;
 
 import java.io.IOException;
@@ -11,6 +19,11 @@ import java.util.Objects;
 import java.util.Optional;
 
 import static fr.umontpellier.evo.utils.Streams.unwrap;
+import static guru.nidi.graphviz.attribute.Attributes.attr;
+import static guru.nidi.graphviz.attribute.Rank.RankDir.LEFT_TO_RIGHT;
+import static guru.nidi.graphviz.model.Factory.graph;
+import static guru.nidi.graphviz.model.Factory.node;
+import static guru.nidi.graphviz.model.Link.to;
 import static java.nio.file.Files.walkFileTree;
 
 @CommandLine.Command(name = "evo")
@@ -30,21 +43,39 @@ public class EvoCommand {
                 .map(p -> p.accept(StatisticVisitor::new))
                 .toList();
 
-        System.out.println("- Nombre de classes dans l'application (sans compter les sous-classes): " + stats.size());
-        System.out.println("- Nombre de lignes de code de l’application: " + stats.stream()
+        System.out.println(Colors.brightGreen + "->" + Colors.reset + " Nombre de classes dans l'application (sans compter les sous-classes) : " + Colors.brightBlue + stats.size() + Colors.reset);
+        System.out.println(Colors.brightGreen + "->" + Colors.reset + " Nombre de lignes de code de l’application :                            " + Colors.brightBlue + stats.stream()
                 .map(StatisticVisitor.Result::clazz)
                 .mapToInt(StatisticVisitor.Result.Class::lineCount)
-                .sum());
-        System.out.println("- Nombre total de méthodes de l’application: " + stats.stream()
+                .sum() + Colors.reset);
+        System.out.println(Colors.brightGreen + "->" + Colors.reset + " Nombre total de méthodes de l’application :                            " + Colors.brightBlue + stats.stream()
                 .map(StatisticVisitor.Result::methods)
                 .mapToInt(List::size)
-                .sum());
-        System.out.println("- Nombre de ligne par méthode moyen: " + stats.stream()
+                .sum() + Colors.reset);
+        System.out.println(Colors.brightGreen + "->" + Colors.reset + " Nombre de ligne par méthode moyen :                                    " + Colors.brightBlue + String.format("%1.2f", stats.stream()
                 .flatMap(p -> p.methods().stream())
                 .mapToInt(StatisticVisitor.Result.Method::lineCount)
                 .average()
-                .orElse(0.0));
+                .orElse(0.0)) + Colors.reset);
 
+        return 0;
+    }
+
+    @CommandLine.Command(name = "callgraph")
+    public Integer callGraph(
+            @CommandLine.Parameters(arity = "1", paramLabel = "root") Path root
+    ) {
+        Graph g = graph("example1").directed()
+                .graphAttr().with(Rank.dir(LEFT_TO_RIGHT))
+                .nodeAttr().with(Font.name("arial"))
+                .linkAttr().with("class", "link-class")
+                .with(
+                        node("a").with(Color.RED).link(node("b")),
+                        node("b").link(
+                                to(node("c")).with(attr("weight", 5), Style.DASHED)
+                        )
+                );
+        System.out.println(Graphviz.fromGraph(g).height(100).render(Format.DOT));
         return 0;
     }
 
